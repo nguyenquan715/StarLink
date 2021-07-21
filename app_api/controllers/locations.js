84ac22b3628ae9076dc4ff0743a9020c3775a87c
nguyenquan715 <quanquan21111999@gmail.com>
2021-07-21 04:03:16 -0600

    Add POST API

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -16,7 +16,39 @@ const getListLocations = (req, res) => {
 };
 
 const createLocation = (req, res) => {
-    
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: {
+            type: "Point",
+            coordinates: [
+                parseFloat(req.body.lng),
+                parseFloat(req.body.lat)
+            ]
+        },
+        openingTimes: [
+            {
+                days: req.body.days1,
+                opening: req.body.opening1,
+                closing: req.body.closing1,
+                closed: req.body.closed1
+             },
+            {
+                days: req.body.days2,
+                opening: req.body.opening2,
+                closing: req.body.closing2,
+                closed: req.body.closed2
+            }
+        ]},
+        (err, location) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(201).json(location);
+            }
+        }
+    );
 };
 
 const getLocationById = (req, res) => {
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
 const createReview = (req, res) => {
-    
+    Loc
+        .findById(req.params.locationId)
+        .select('reviews')
+        .exec((err, location) => {
+            if(!location){
+                return res.status(404).json({message: 'Location not found!'});
+            } else if(err){
+                return res.status(404).json(err);
+            }
+            doAddReview(req, res, location);
+        });
 };
 
 const getReviewById = (req, res) => {
@@ -40,6 +50,23 @@ const updateReviewById = (req, res) => {
 const deleteReviewById = (req, res) => {
     
 };
+
+const doAddReview = (req, res, location) => {
+    if(!location){
+        return res.status(404).json({message: 'Location not found!'});
+    }
+    const {author, rating, reviewText} = req.body;
+    location.reviews.push({
+        author,
+        rating,
+        reviewText
+    });
+    location.save((err, location) => {
+        if(err){
+            return res.status(400).json(err);
+        }
+    });
+}
 
 module.exports = {
     createReview,
